bffbd2afbe4e6f85b9e7025b4241f4819d8f3758
Priyaraj7 <suppichala@gmail.com>
2021-11-03 14:28:12 -0700

    initial commit

---
+++ tests/routes/root.route.test.js
@@ -0,0 +1,18 @@
+const build = require('../../src/app');
+
+describe('Root route', () => {
+  beforeEach(() => {
+    app = build();
+  });
+
+  afterEach(() => {
+    app.close();
+  });
+  it('should return 200 when root route called', async () => {
+    const res = await app.inject({
+      url: '/',
+    });
+    expect(res.statusCode).toBe(200);
+    expect(res.json()).toEqual({ hello: 'world!' });
+  });
+});
