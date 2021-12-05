81a6ec70bad233d4cbebea8b0ebf4dbdfbb2e638
Priyaraj7 <suppichala@gmail.com>
2021-12-04 20:14:57 -0800

    cross check

--- tests/routes/root.route.test.js
+++ tests/routes/root.route.test.js
@@ -14,6 +14,6 @@ describe('Root route', () => {
       url: '/',
     });
     expect(res.statusCode).toBe(200);
-    expect(res.json()).toEqual({ hello: 'world!' });
+    expect(res.json()).toEqual({ hello: 'world!- check' });
   });
 });
